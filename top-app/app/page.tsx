efb3b6aae4cbe2a4b6f593fb11e96ce7ba2ae26f
VladislavChurbakov <v.churbakov@voskhod.ru>
2025-07-04 10:44:14 +0800

    Сделал ДЗ 10.12

--- top-app/app/page.tsx
+++ top-app/app/page.tsx
@@ -1,19 +1,19 @@
 'use client';
 
 import {Card, Like_Changed} from "@/components";
-
+import styles from "./page.module.css";
 
 
 
 export default function Home() {
     return (
-        <div>
-            <div style={{ display: 'flex'}}>
-                <Card />
+        <div className={styles.wrapper}>
+                <Card className={styles.card}/>
+                <Card className={styles.card}/>
+                <Card className={styles.card}/>
                 <Like_Changed isActive={false} />
-            </div>
 
-        </div>
+         </div>
     );
 }
 
