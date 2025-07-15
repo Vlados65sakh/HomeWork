54d9528b7c64f09c62b74c4f7ef74eee50d11978
VladislavChurbakov <v.churbakov@voskhod.ru>
2025-07-15 12:52:05 +0800

    Выполнение 3 домашки

--- top-app/components/Main/Header/Header.tsx
+++ top-app/components/Main/Header/Header.tsx
@@ -2,6 +2,7 @@ import cn from "classnames";
 import styles from "@/components/Main/Header/Header.module.css";
 import HeaderIcon from "@/components/Main/Header/Header.svg";
 import {ReactElement} from "react";
+import Link from "next/link";
 
 interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {
     className?: string;
@@ -12,7 +13,9 @@ export const Header = ({className, ...props}: HeaderProps): ReactElement => {
         <div {...props} className={cn(styles.header, className)}>
             .my_blog
             <div className={styles.iconWrapper}>
+                <Link href={"https://github.com/Vlados65sakh/HomeWork"}>
                 <HeaderIcon/>
+                </Link>
             </div>
         </div>
     );
