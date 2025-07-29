ada2b64d2e26073f82be627eeb62f3e2bb0d2104
VladislavChurbakov <v.churbakov@voskhod.ru>
2025-07-29 11:05:53 +0800

    Выполнение 4 домашки

---
+++ top-app/app/(site)/post/page.tsx
@@ -0,0 +1,10 @@
+import styles from "@/app/page.module.css";
+import {Post} from "@/components";
+
+export default async function Postpage() {
+    return (
+        <div>
+            <Post/>
+        </div>
+    );
+}
--- top-app/components/index.ts
+++ top-app/components/index.ts
@@ -3,3 +3,4 @@ export * from './Card/Card';
 export * from './Like/Like';
 export * from '@/components/Like_Changed/Like_Changed';
 export * from './Main/Header/Header';
+export * from './Post/Post';
